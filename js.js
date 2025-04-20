775901d45f53d38d657571e6d820b050f919bded
Maha38172 <maha.38172@gmail.com>
2025-04-20 12:06:16 -0700

    first

---
+++ js.js
@@ -0,0 +1,22 @@
+"use strict";
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+    const {latitude}=position.coords;
+    const {longitude}=position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude},12z?entry=ttu&g_ep=EgoyMDI1MDQxMy4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D`);
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+    },function(){
+        alert("cant acess your")
+    })
+}
+
+
+alert("maha mohamed huiguihioj")
